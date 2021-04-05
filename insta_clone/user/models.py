c1400129f859d52f8ef23bb10d62717d1799bea0
mst101097 <singh.1097.mohit@gmail.com>
2021-04-05 23:31:36 +0530

    project setup and Apps created

---
+++ insta_clone/user/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.base_user import BaseUserManager
+
+# Create your models here.
+class User(AbstractUser):
+    picture = models.ImageField(upload_to='profile_pictures', null=False, blank=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    # objects = BaseUserManager()
+
+    # AUTH_USER_MODEL  ='user.User'
